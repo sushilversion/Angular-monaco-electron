a8258930ea2dd769817e20c212cd3ae2612fcb93
Sushil Pandey <sushilpandey@Sushils-MacBook-Air.local>
2018-12-23 00:44:46 +0530

    Added monaco editor and verified on browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MonacoFile } from 'ngx-monaco'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  file: MonacoFile = {
+    uri: 'index.js',
+    language: 'javascript',
+    content: `console.log('hello world');`
+  };
+
+  onFileChange(file: MonacoFile){
+    //handle file change
+    console.log('File Change invoked');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MonacoEditorModule, COMPLETION_PROVIDERS } from 'ngx-monaco';
 
 import { AppComponent } from './app.component';
+import { TravisCompletionProvider } from './providers/travis-completion.provider';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MonacoEditorModule.forRoot()
   ],
-  providers: [],
+  providers: [
+     { provide: COMPLETION_PROVIDERS, useClass: TravisCompletionProvider, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
